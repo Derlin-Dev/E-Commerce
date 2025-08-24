736212671a57d1bf1fea407b38b0fbf19efdf6c6
Derlin-Dev <derlinp132@gmail.com>
2025-08-23 21:11:48 -0400

    Subiendo las carpetas iniciales

---
+++ Cart-services/src/main/java/com/E_Commerce/Cart_services/CartServicesApplication.java
@@ -0,0 +1,13 @@
+package com.E_Commerce.Cart_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartServicesApplication.class, args);
+	}
+
+}
---
+++ Product-services/src/test/java/com/E_Commerce/Product_services/ProductServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.E_Commerce.Product_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
